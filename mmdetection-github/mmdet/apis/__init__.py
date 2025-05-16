8f01877d665772a134939a4ae1ced3b90ca346a4
Tadeáš Báča <bacatade@fit.cvut.cz>
2025-05-16 22:08:54 +0200

    Experimenting with fixing the repo

--- mmdetection-github/mmdet/apis/__init__.py
+++ mmdetection-github/mmdet/apis/__init__.py
@@ -1,16 +1,30 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .inference import (async_inference_detector, inference_detector,
-                        init_detector, init_detector_pipeline, show_result_pyplot, show_result_pyplot_part, show_result_pyplot_part100x)
+from .inference import (
+    async_inference_detector,
+    inference_detector,
+    init_detector,
+    init_detector_pipeline,
+    show_result_pyplot,
+    show_result_pyplot_part,
+    show_result_pyplot_part100x,
+)
 from .test import multi_gpu_test, single_gpu_test
-from .train import (get_root_logger, init_random_seed, set_random_seed,
-                    train_detector)
-from .train_pipeline import (train_detector_pipeline)
-from .pipeline_parallel import (PipelineParallel)
+from .train import get_root_logger, init_random_seed, set_random_seed, train_detector
 
 __all__ = [
-    'get_root_logger', 'set_random_seed', 'train_detector', 'train_detector_pipeline', 'init_detector', 
-    'init_detector_pipeline',
-    'async_inference_detector', 'inference_detector', 'show_result_pyplot', 'show_result_pyplot_part',
-    'show_result_pyplot_part100x',
-    'multi_gpu_test', 'single_gpu_test', 'init_random_seed', 'PipelineParallel'
+    "get_root_logger",
+    "set_random_seed",
+    "train_detector",
+    "train_detector_pipeline",
+    "init_detector",
+    "init_detector_pipeline",
+    "async_inference_detector",
+    "inference_detector",
+    "show_result_pyplot",
+    "show_result_pyplot_part",
+    "show_result_pyplot_part100x",
+    "multi_gpu_test",
+    "single_gpu_test",
+    "init_random_seed",
+    "PipelineParallel",
 ]
