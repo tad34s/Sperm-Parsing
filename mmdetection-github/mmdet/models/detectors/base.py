39374abaafa15244f294cda3647f55091126dadb
Tadeáš Báča <bacatade@fit.cvut.cz>
2025-06-16 12:17:23 +0200

    debug

--- mmdetection-github/mmdet/models/detectors/base.py
+++ mmdetection-github/mmdet/models/detectors/base.py
@@ -8,8 +8,13 @@ import torch
 import torch.distributed as dist
 from mmcv.runner import BaseModule, auto_fp16
 
-from mmdet.core.visualization import imshow_det_bboxes, imshow_det_bboxes_part, imshow_det_bboxes_part100x
+from mmdet.core.visualization import (
+    imshow_det_bboxes,
+    imshow_det_bboxes_part,
+    imshow_det_bboxes_part100x,
+)
 import pdb
+
 
 class BaseDetector(BaseModule, metaclass=ABCMeta):
     """Base class for detectors."""
@@ -21,26 +26,28 @@ class BaseDetector(BaseModule, metaclass=ABCMeta):
     @property
     def with_neck(self):
         """bool: whether the detector has a neck"""
-        return hasattr(self, 'neck') and self.neck is not None
+        return hasattr(self, "neck") and self.neck is not None
 
     # TODO: these properties need to be carefully handled
     # for both single stage & two stage detectors
     @property
     def with_shared_head(self):
         """bool: whether the detector has a shared head in the RoI Head"""
-        return hasattr(self, 'roi_head') and self.roi_head.with_shared_head
+        return hasattr(self, "roi_head") and self.roi_head.with_shared_head
 
     @property
     def with_bbox(self):
         """bool: whether the detector has a bbox head"""
-        return ((hasattr(self, 'roi_head') and self.roi_head.with_bbox)
-                or (hasattr(self, 'bbox_head') and self.bbox_head is not None))
+        return (hasattr(self, "roi_head") and self.roi_head.with_bbox) or (
+            hasattr(self, "bbox_head") and self.bbox_head is not None
+        )
 
     @property
     def with_mask(self):
         """bool: whether the detector has a mask head"""
-        return ((hasattr(self, 'roi_head') and self.roi_head.with_mask)
-                or (hasattr(self, 'mask_head') and self.mask_head is not None))
+        return (hasattr(self, "roi_head") and self.roi_head.with_mask) or (
+            hasattr(self, "mask_head") and self.mask_head is not None
+        )
 
     @abstractmethod
     def extract_feat(self, imgs):
@@ -77,7 +84,7 @@ class BaseDetector(BaseModule, metaclass=ABCMeta):
         # then used for the transformer_head.
         batch_input_shape = tuple(imgs[0].size()[-2:])
         for img_meta in img_metas:
-            img_meta['batch_input_shape'] = batch_input_shape
+            img_meta["batch_input_shape"] = batch_input_shape
 
     async def async_simple_test(self, img, img_metas, **kwargs):
         raise NotImplementedError
@@ -92,14 +99,16 @@ class BaseDetector(BaseModule, metaclass=ABCMeta):
         pass
 
     async def aforward_test(self, *, img, img_metas, **kwargs):
-        for var, name in [(img, 'img'), (img_metas, 'img_metas')]:
+        for var, name in [(img, "img"), (img_metas, "img_metas")]:
             if not isinstance(var, list):
-                raise TypeError(f'{name} must be a list, but got {type(var)}')
+                raise TypeError(f"{name} must be a list, but got {type(var)}")
 
         num_augs = len(img)
         if num_augs != len(img_metas):
-            raise ValueError(f'num of augmentations ({len(img)}) '
-                             f'!= num of image metas ({len(img_metas)})')
+            raise ValueError(
+                f"num of augmentations ({len(img)}) "
+                f"!= num of image metas ({len(img_metas)})"
+            )
         # TODO: remove the restriction of samples_per_gpu == 1 when prepared
         samples_per_gpu = img[0].size(0)
         assert samples_per_gpu == 1
@@ -119,14 +128,16 @@ class BaseDetector(BaseModule, metaclass=ABCMeta):
                 augs (multiscale, flip, etc.) and the inner list indicates
                 images in a batch.
         """
-        for var, name in [(imgs, 'imgs'), (img_metas, 'img_metas')]:
+        for var, name in [(imgs, "imgs"), (img_metas, "img_metas")]:
             if not isinstance(var, list):
-                raise TypeError(f'{name} must be a list, but got {type(var)}')
+                raise TypeError(f"{name} must be a list, but got {type(var)}")
 
         num_augs = len(imgs)
         if num_augs != len(img_metas):
-            raise ValueError(f'num of augmentations ({len(imgs)}) '
-                             f'!= num of image meta ({len(img_metas)})')
+            raise ValueError(
+                f"num of augmentations ({len(imgs)}) "
+                f"!= num of image meta ({len(img_metas)})"
+            )
 
         # NOTE the batched image size information may be useful, e.g.
         # in DETR, this is needed for the construction of masks, which is
@@ -134,7 +145,7 @@ class BaseDetector(BaseModule, metaclass=ABCMeta):
         for img, img_meta in zip(imgs, img_metas):
             batch_size = len(img_meta)
             for img_id in range(batch_size):
-                img_meta[img_id]['batch_input_shape'] = tuple(img.size()[-2:])
+                img_meta[img_id]["batch_input_shape"] = tuple(img.size()[-2:])
 
         if num_augs == 1:
             # proposals (List[List[Tensor]]): the outer list indicates
@@ -142,18 +153,18 @@ class BaseDetector(BaseModule, metaclass=ABCMeta):
             # indicates images in a batch.
             # The Tensor should have a shape Px4, where P is the number of
             # proposals.
-            if 'proposals' in kwargs:
-                kwargs['proposals'] = kwargs['proposals'][0]
+            if "proposals" in kwargs:
+                kwargs["proposals"] = kwargs["proposals"][0]
             return self.simple_test(imgs[0], img_metas[0], **kwargs)
         else:
-            assert imgs[0].size(0) == 1, 'aug test does not support ' \
-                                         'inference with batch size ' \
-                                         f'{imgs[0].size(0)}'
+            assert imgs[0].size(0) == 1, (
+                f"aug test does not support inference with batch size {imgs[0].size(0)}"
+            )
             # TODO: support test augmentation for predefined proposals
-            assert 'proposals' not in kwargs
+            assert "proposals" not in kwargs
             return self.aug_test(imgs, img_metas, **kwargs)
 
-    @auto_fp16(apply_to=('img', ))
+    @auto_fp16(apply_to=("img",))
     def forward(self, img, img_metas, return_loss=True, **kwargs):
         """Calls either :func:`forward_train` or :func:`forward_test` depending
         on whether ``return_loss`` is ``True``.
@@ -192,23 +203,25 @@ class BaseDetector(BaseModule, metaclass=ABCMeta):
             elif isinstance(loss_value, list):
                 log_vars[loss_name] = sum(_loss.mean() for _loss in loss_value)
             else:
-                raise TypeError(
-                    f'{loss_name} is not a tensor or list of tensors')
+                raise TypeError(f"{loss_name} is not a tensor or list of tensors")
 
-        loss = sum(_value for _key, _value in log_vars.items()
-                   if 'loss' in _key)
+        loss = sum(_value for _key, _value in log_vars.items() if "loss" in _key)
 
         # If the loss_vars has different length, GPUs will wait infinitely
         if dist.is_available() and dist.is_initialized():
             log_var_length = torch.tensor(len(log_vars), device=loss.device)
             dist.all_reduce(log_var_length)
-            message = (f'rank {dist.get_rank()}' +
-                       f' len(log_vars): {len(log_vars)}' + ' keys: ' +
-                       ','.join(log_vars.keys()))
-            assert log_var_length == len(log_vars) * dist.get_world_size(), \
-                'loss log variables are different across GPUs!\n' + message
+            message = (
+                f"rank {dist.get_rank()}"
+                + f" len(log_vars): {len(log_vars)}"
+                + " keys: "
+                + ",".join(log_vars.keys())
+            )
+            assert log_var_length == len(log_vars) * dist.get_world_size(), (
+                "loss log variables are different across GPUs!\n" + message
+            )
 
-        log_vars['loss'] = loss
+        log_vars["loss"] = loss
         for loss_name, loss_value in log_vars.items():
             # reduce loss when distributed training
             if dist.is_available() and dist.is_initialized():
@@ -248,8 +261,7 @@ class BaseDetector(BaseModule, metaclass=ABCMeta):
         losses = self(**data)
         loss, log_vars = self._parse_losses(losses)
 
-        outputs = dict(
-            loss=loss, log_vars=log_vars, num_samples=len(data['img_metas']))
+        outputs = dict(loss=loss, log_vars=log_vars, num_samples=len(data["img_metas"]))
 
         return outputs
 
@@ -263,24 +275,25 @@ class BaseDetector(BaseModule, metaclass=ABCMeta):
         losses = self(**data)
         loss, log_vars = self._parse_losses(losses)
 
-        outputs = dict(
-            loss=loss, log_vars=log_vars, num_samples=len(data['img_metas']))
+        outputs = dict(loss=loss, log_vars=log_vars, num_samples=len(data["img_metas"]))
 
         return outputs
 
-    def show_result(self,
-                    img,
-                    result,
-                    score_thr=0.3,
-                    bbox_color=(72, 101, 241),
-                    text_color=(72, 101, 241),
-                    mask_color=None,
-                    thickness=2,
-                    font_size=13,
-                    win_name='',
-                    show=False,
-                    wait_time=0,
-                    out_file=None):
+    def show_result(
+        self,
+        img,
+        result,
+        score_thr=0.3,
+        bbox_color=(72, 101, 241),
+        text_color=(72, 101, 241),
+        mask_color=None,
+        thickness=2,
+        font_size=13,
+        win_name="",
+        show=False,
+        wait_time=0,
+        out_file=None,
+    ):
         """Draw `result` over `img`.
 
         Args:
@@ -335,6 +348,7 @@ class BaseDetector(BaseModule, metaclass=ABCMeta):
         if out_file is not None:
             show = False
         # draw bounding boxes
+        print("segments:", segms, "labels", labels, sep="\n")
         img = imshow_det_bboxes(
             img,
             bboxes,
@@ -350,24 +364,27 @@ class BaseDetector(BaseModule, metaclass=ABCMeta):
             win_name=win_name,
             show=show,
             wait_time=wait_time,
-            out_file=out_file)
+            out_file=out_file,
+        )
 
         if not (show or out_file):
             return img
 
-    def show_result_part(self,
-                    img,
-                    result,
-                    score_thr=0.5,
-                    bbox_color=(72, 101, 241),
-                    text_color=(72, 101, 241),
-                    mask_color=None,
-                    thickness=2,
-                    font_size=13,
-                    win_name='',
-                    show=False,
-                    wait_time=0,
-                    out_file=None):
+    def show_result_part(
+        self,
+        img,
+        result,
+        score_thr=0.5,
+        bbox_color=(72, 101, 241),
+        text_color=(72, 101, 241),
+        mask_color=None,
+        thickness=2,
+        font_size=13,
+        win_name="",
+        show=False,
+        wait_time=0,
+        out_file=None,
+    ):
         """Draw `result` over `img`.
 
         Args:
@@ -411,10 +428,12 @@ class BaseDetector(BaseModule, metaclass=ABCMeta):
         ]
         labels = np.concatenate(labels)
         # draw segmentation masks
-        for i in range(0,3):
+        for i in range(0, 3):
             if segm_result[i] is not None and len(labels) > 0:  # non empty
                 if isinstance(segm_result[i][0], torch.Tensor):
-                    segm_result[i] = torch.stack(segm_result[i], dim=0).detach().cpu().numpy()
+                    segm_result[i] = (
+                        torch.stack(segm_result[i], dim=0).detach().cpu().numpy()
+                    )
                 else:
                     segm_result[i] = np.stack(segm_result[i], axis=0)
         # if out_file specified, do not show image in window
@@ -436,24 +455,27 @@ class BaseDetector(BaseModule, metaclass=ABCMeta):
             win_name=win_name,
             show=show,
             wait_time=wait_time,
-            out_file=out_file)
+            out_file=out_file,
+        )
 
         if not (show or out_file):
             return img
 
-    def show_result_part100x(self,
-                    img,
-                    result,
-                    score_thr=0.5,
-                    bbox_color=(72, 101, 241),
-                    text_color=(72, 101, 241),
-                    mask_color=None,
-                    thickness=2,
-                    font_size=13,
-                    win_name='',
-                    show=False,
-                    wait_time=0,
-                    out_file=None):
+    def show_result_part100x(
+        self,
+        img,
+        result,
+        score_thr=0.5,
+        bbox_color=(72, 101, 241),
+        text_color=(72, 101, 241),
+        mask_color=None,
+        thickness=2,
+        font_size=13,
+        win_name="",
+        show=False,
+        wait_time=0,
+        out_file=None,
+    ):
         """Draw `result` over `img`.
 
         Args:
@@ -482,7 +504,7 @@ class BaseDetector(BaseModule, metaclass=ABCMeta):
         Returns:
             img (Tensor): Only if not `show` or `out_file`
         """
-        #pdb.set_trace()
+        # pdb.set_trace()
         img = mmcv.imread(img)
         img = img.copy()
         if isinstance(result, tuple):
@@ -492,21 +514,23 @@ class BaseDetector(BaseModule, metaclass=ABCMeta):
         else:
             bbox_result, segm_result = result, None
         bboxes = np.vstack(bbox_result)
-        #pdb.set_trace()
+        # pdb.set_trace()
         labels = [
             np.full(bbox.shape[0], i, dtype=np.int32)
             for i, bbox in enumerate(bbox_result)
         ]
         labels = np.concatenate(labels)
         # draw segmentation masks
-        #pdb.set_trace()
-        for i in range(0,5):
+        # pdb.set_trace()
+        for i in range(0, 5):
             if segm_result[i] is not None and len(labels) > 0:  # non empty
                 if isinstance(segm_result[i][0], torch.Tensor):
-                    segm_result[i] = torch.stack(segm_result[i], dim=0).detach().cpu().numpy()
+                    segm_result[i] = (
+                        torch.stack(segm_result[i], dim=0).detach().cpu().numpy()
+                    )
                 else:
                     segm_result[i] = np.stack(segm_result[i], axis=0)
-        #segm_result[0] = np.stack(segm_result[0], axis=0)
+        # segm_result[0] = np.stack(segm_result[0], axis=0)
         # if out_file specified, do not show image in window
         if out_file is not None:
             show = False
@@ -526,11 +550,13 @@ class BaseDetector(BaseModule, metaclass=ABCMeta):
             win_name=win_name,
             show=show,
             wait_time=wait_time,
-            out_file=out_file)
+            out_file=out_file,
+        )
 
         if not (show or out_file):
             return img
 
     def onnx_export(self, img, img_metas):
-        raise NotImplementedError(f'{self.__class__.__name__} does '
-                                  f'not support ONNX EXPORT')
+        raise NotImplementedError(
+            f"{self.__class__.__name__} does not support ONNX EXPORT"
+        )
