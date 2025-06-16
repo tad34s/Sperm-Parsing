cea99a2a27c591d56dd88ad352f5d3de86ecddeb
Tadeáš Báča <bacatade@fit.cvut.cz>
2025-06-16 13:11:18 +0200

    eval fix

--- mmdetection-github/docker/run.sh
+++ mmdetection-github/docker/run.sh
@@ -5,3 +5,9 @@ docker run -it \
   -v $(pwd)/container:/sperm \
   sperm-detection \
   bash -c "cd /sperm/mmdetection-github && python tools/test.py configs/git_fusionrcnn/cascade_mask_rcnn_r101_caffe_feature_blend_coarse_fine_edge_fpn_1x_spermparsingeval.py epoch_35.pth --eval bbox segm"
+
+
+
+
+
+
