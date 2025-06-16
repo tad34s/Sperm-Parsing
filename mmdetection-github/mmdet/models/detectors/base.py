dfb95640108f95b61d94dbe8b251b02d5ca2fec8
Tadeáš Báča <bacatade@fit.cvut.cz>
2025-06-16 12:36:39 +0200

    debug

--- mmdetection-github/mmdet/models/detectors/base.py
+++ mmdetection-github/mmdet/models/detectors/base.py
@@ -348,7 +348,7 @@ class BaseDetector(BaseModule, metaclass=ABCMeta):
         if out_file is not None:
             show = False
         # draw bounding boxes
-        print("segments:", segms, "labels", labels, sep="\n")
+        print("segments:", segms.shape, "labels", labels.shape, sep="\n")
         img = imshow_det_bboxes(
             img,
             bboxes,
