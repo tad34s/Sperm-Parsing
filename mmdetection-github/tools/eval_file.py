a78e2a30e8ece872e1255afa51e4bd1b7ec6a131
Tadeáš Báča <bacatade@fit.cvut.cz>
2025-07-06 13:53:17 +0200

    changing eval script

--- mmdetection-github/tools/eval_file.py
+++ mmdetection-github/tools/eval_file.py
@@ -5,7 +5,11 @@ from mmdet.apis import inference_detector, init_detector
 
 
 def test_image(
-    config_file, checkpoint_file, image_path, output_path=None, device="cuda:0"
+    config_file,
+    checkpoint_file,
+    inference_image,
+    output_path=None,
+    device="cuda:0",
 ):
     """
     Run inference on a new image using a pretrained model and visualize the results.
@@ -17,9 +21,9 @@ def test_image(
         output_path (str, optional): Path to save the visualized result. Defaults to None.
         device (str): Device to run the model on ('cuda:0' for GPU or 'cpu'). Defaults to 'cuda:0'.
     """
-    print(f"Testing image: {image_path}")
+    print(f"Testing image: {inference_image}")
     model = init_detector(config_file, checkpoint_file, device=device)
-    result = inference_detector(model, image_path)
+    result = inference_detector(model, inference_image)
 
     if isinstance(result, tuple):
         bbox_result, segm_result = result
@@ -31,7 +35,9 @@ def test_image(
     bboxes = np.vstack(bbox_result)
     np.save("bboxes.npy", bboxes)
 
-    model.show_result_part100x(image_path, result, score_thr=0.5, out_file=output_path)
+    model.show_result_part100x(
+        inference_image, result, score_thr=0.5, out_file=output_path
+    )
     print(f"Result saved to: {output_path}")
 
 
@@ -52,7 +58,7 @@ if __name__ == "__main__":
     test_image(
         config_file=args.config,
         checkpoint_file=args.checkpoint,
-        image_path=args.image,
+        inference_image=args.image,
         output_path=args.output,
         device=args.device,
     )
